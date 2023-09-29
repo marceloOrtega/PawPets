62456601be42af748e223a2b4b2d811485038d1c
Marelo668 <110124300+marceloOrtega@users.noreply.github.com>
2023-09-28 23:25:55 -0300

    New

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -1,40 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const loginForm = document.getElementById('login-form');
-    const errorMessage = document.getElementById('error-message');
-  
-    loginForm.addEventListener('submit', async function (e) {
-      e.preventDefault();
-  
-    
-      const email = document.getElementById('email').value;
-      const senha = document.getElementById('senha').value;
-  
-      const userData = {
-        email: email,
-        senha: senha,
-      };
-  
-      try {
-        const response = await fetch('http://localhost:3000/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(userData),
-        });
-  
-        if (response.ok) {
-        
-          const responseData = await response.json();
-          window.localStorage.setItem('token', responseData.token);
-          window.location.href = '.html'; 
-        } else {
-          const errorData = await response.json();
-          errorMessage.textContent = errorData.error;
-        }
-      } catch (error) {
-        console.error('Erro ao fazer login:', error);
+const emailInput = document.getElementById('email');
+const senhaInput = document.getElementById('senha');
+const loginButton = document.getElementById('login');
+
+loginButton.addEventListener('click', () => {
+  const email = emailInput.value;
+  const senha = senhaInput.value;
+
+  // Aqui você pode fazer uma solicitação AJAX para o backend para verificar o login.
+  // Substitua 'URL_DO_BACKEND' pela URL real do seu backend de login.
+
+  fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, senha }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.mensagem === 'Login bem-sucedido') {
+        // Redirecionar ou realizar outras ações após o login bem-sucedido.
+        console.log('Login bem-sucedido:', data.usuario);
+      } else {
+        // Tratar erros de login, como senha incorreta ou usuário não encontrado.
+        console.error('Erro de login:', data.erro);
       }
+    })
+    .catch((error) => {
+      console.error('Erro ao realizar login:', error);
     });
-  });
-  
+});
