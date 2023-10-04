a88828e934d8bcced456ef20bc4f9b0082df51fd
marceloOrtega <eumarceloortega2016@gmail.com>
2023-10-04 11:24:52 -0300

    new

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,27 +1,27 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-const jwt = require('jsonwebtoken');
+// const { PrismaClient } = require('@prisma/client');
+// const prisma = new PrismaClient();
+// const jwt = require('jsonwebtoken');
 
-const verificarToken = async (req, res, next) => {
-  const token = req.header('Authorization');
+// const verificarToken = async (req, res, next) => {
+//   const token = req.header('Authorization');
 
-  if (!token) {
-    return res.status(401).json({ erro: 'Token não fornecido' });
-  }
+//   if (!token) {
+//     return res.status(401).json({ erro: 'Token não fornecido' });
+//   }
 
-  try {
-    const tokenSemBearer = token.replace('Bearer ', '');
-    const decoded = jwt.verify(tokenSemBearer, process.env.SECRET_JWT);
-    req.usuario = decoded;
-    await prisma.usuario.update({
-      where: { id: decoded.id },
-      data: { verificado: true },
-    });
+//   try {
+//     const tokenSemBearer = token.replace('Bearer ', '');
+//     const decoded = jwt.verify(tokenSemBearer, process.env.SECRET_JWT);
+//     req.usuario = decoded;
+//     await prisma.usuario.update({
+//       where: { id: decoded.id },
+//       data: { verificado: true },
+//     });
 
-    next();
-  } catch (error) {
-    res.status(401).json({ erro: 'Token inválido' });
-  }
-};
+//     next();
+//   } catch (error) {
+//     res.status(401).json({ erro: 'Token inválido' });
+//   }
+// };
 
-module.exports = verificarToken;
+// module.exports = verificarToken;
