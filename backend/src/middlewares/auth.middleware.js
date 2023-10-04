29f2f4adca634fdc24eafb0e918d6e785a4337a0
marceloOrtega <eumarceloortega2016@gmail.com>
2023-10-04 09:10:32 -0300

    verification

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const jwt = require('jsonwebtoken');
+
+const verificarToken = async (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ erro: 'Token não fornecido' });
+  }
+
+  try {
+    const tokenSemBearer = token.replace('Bearer ', '');
+    const decoded = jwt.verify(tokenSemBearer, process.env.SECRET_JWT);
+
+    // O usuário autenticado está disponível em req.usuario
+    req.usuario = decoded;
+
+    // Agora, você pode atualizar o campo 'verificado' no banco de dados para true
+    // Certifique-se de que o nome do campo no banco de dados corresponda ao nome no seu modelo Prisma (no caso, 'verificado')
+    await prisma.usuario.update({
+      where: { id: decoded.id },
+      data: { verificado: true },
+    });
+
+    next();
+  } catch (error) {
+    res.status(401).json({ erro: 'Token inválido' });
+  }
+};
+
+module.exports = verificarToken;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Usuarios = require('./controllers/user.controller');
 const Pets = require('./controllers/pets.controller');
 const Vacinas = require('./controllers/vacinas.controller');
+const verificarToken = require('./middlewares/auth.middleware');
 
 router.get('/', (req, res) => { res.json('Sistema Online').end() });
 
@@ -11,6 +12,11 @@ router.post('/cadastrar', Usuarios.cadastrar);
 router.get('/usuarios', Usuarios.listar);
 router.get('/buscar/:id', Usuarios.buscar);
 router.post('/login', Usuarios.login);
+
+router.get('/recurso-protegido', verificarToken, (req, res) => {
+    // O usuário autenticado está disponível em req.usuario
+    res.status(200).json({ mensagem: 'Acesso ao recurso protegido concedido', usuario: req.usuario });
+  });
 
 router.get('/pets', Pets.listarPets);
 router.get('/pets/buscar/:id', Pets.buscarPet);
